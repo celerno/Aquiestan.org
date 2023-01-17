eaaa7444e898ab258882c56c6e5be546aef02b67
celerno <celerno@gmail.com>
2023-01-17 15:14:54 -0700

    Adding SiteType and ignoring local content in upload folder

--- aquiestan.web/Program.cs
+++ aquiestan.web/Program.cs
@@ -4,6 +4,7 @@ using Piranha.AttributeBuilder;
 using Piranha.AspNetCore.Identity.SQLite;
 using Piranha.Data.EF.SQLite;
 using Piranha.Manager.Editor;
+using aquiestan.web.Site;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -62,6 +63,7 @@ app.UsePiranha(options =>
     // Build content types
     new ContentTypeBuilder(options.Api)
         .AddAssembly(typeof(Program).Assembly)
+        .AddType(typeof(AquiestanSite))
         .Build()
         .DeleteOrphans();
 
